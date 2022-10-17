46e87e1dfb2a8fdf7e21f89e16559b976481c909
Florin Mavroian <florin.mavroian@asurion.com>
2022-10-17 22:29:14 +0300

    publicchanged logo og

--- src/components/shared/PageHelmet.js
+++ src/components/shared/PageHelmet.js
@@ -13,7 +13,7 @@ const PageHelmet = ({ pageTitle }) => {
         <meta property="og:url" content="https://www.hewitt-barnes.com" />
         <meta property="og:description" content="Our team of advisors and collaborators have worked with clinicians, materials management, procurement and finance in the UK for many years. We are united in our passion for excellence in the provision of beneficial technology to our healthcare partners." />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://hewitt-barnes-consulting-space.fra1.cdn.digitaloceanspaces.com/logo_small_og.png" />
+        <meta property="og:image" content="https://hewitt-barnes-consulting-space.fra1.cdn.digitaloceanspaces.com/logo-og.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
